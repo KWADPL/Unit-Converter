6a678f6112949b8fdb409481bdfd08be49a791fd
KWADPL <wadeckakarolina@gmail.com>
2024-03-18 17:26:17 +0100

    Finished

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-const inputValue = 10;
 const milesToKm = true;
 let result = 0;
-if(milesToKm){
-   result = inputValue * 1.60934;
-}else{
-    result = inputValue % 1.60934;
+
+function convert(){
+    const inputValue = document.getElementById('user').value;
+    const unit = document.getElementById('unit').value;
+    const milesToKm = unit === "milesToKm";
+    if(milesToKm){
+        result = inputValue * 1.60934;
+     }else{
+         result = inputValue % 1.60934;
+     }
+ document.getElementById('resultElement').value = result;
+    document.getElementById('resultElement').innerHTML = result;
 }
-const resultString = inputValue + " miles are " + result + "km";
 
-console.log(resultString);
-
-const resultElement = document.getElementById('resultElement');
-resultElement.innerHTML = resultString;
+ 
